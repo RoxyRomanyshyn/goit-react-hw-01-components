251f77cfb6a36e83682eda402e7abe816e9fc05a
RoxyRomanyshyn <iamroksa@icloud.com>
2020-09-09 21:31:43 +0300

    Updates

---
+++ precache-manifest.1f8eb798d6b1779cd4e6837fe27b0b7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47ed31ad41453d60200c22721da1641",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "7dbfd27261bd72ca6afe",
+    "url": "/goit-react-hw-01-components/static/css/main.a7617006.chunk.css"
+  },
+  {
+    "revision": "a289453dba5ad227a0f4",
+    "url": "/goit-react-hw-01-components/static/js/2.12880a99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-01-components/static/js/2.12880a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dbfd27261bd72ca6afe",
+    "url": "/goit-react-hw-01-components/static/js/main.942ea7e6.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
